c80ffba840146af9cae270a2aecfc52c827ceb74
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2025-09-19 17:57:01 +0200

    chore(accommodation-service): initial project setup

---
+++ Backend/Accommodations/src/main/java/org/example/accommodations/AccommodationsApplication.java
@@ -0,0 +1,13 @@
+package org.example.accommodations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccommodationsApplication.class, args);
+    }
+
+}
